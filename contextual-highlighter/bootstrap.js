7279b4c65c9863b1ddaefd88c638ff462f19ad0f
peytonrapo <peytonrapo01@gmail.com>
2026-03-18 12:45:23 -0700

    got the code to work in Zotero; now I'm just setting up the code itself

---
+++ contextual-highlighter/bootstrap.js
@@ -0,0 +1,118 @@
+var MyPlugin;
+
+function install() {}
+function uninstall() {}
+
+async function startup({ id, version, rootURI }) {
+  MyPlugin = new class {
+    init({id, version, rootURI}) {
+      this.id = id;
+      this.version = version;
+      this.rootURI = rootURI;
+      this.activeHighlights = [];
+      this.unsetAnnotations = null;
+      this.tabObserverID = Zotero.Notifier.registerObserver({
+        notify: (event, type, ids, extraData) => {
+          Services.console.logStringMessage(`[MyPlugin] ${event} on ${type}: ${JSON.stringify(ids)}`);
+          if (event === "select" || event === "close") {
+            // unset annotations if present.
+            if (this.unsetAnnotations && this.activeHighlights) {
+              this.unsetAnnotations(this.activeHighlights);
+              this.activeHighlights = [];
+            }
+          }
+          if (event === "select" || event === "load") {
+            // add annotations if possible
+            _addTabHighlights(ids[0]);
+          }
+        }
+      }, ["tab"]);
+      Services.console.logStringMessage(`MyPlugin initialized with id: ${id}, version: ${version}, rootURI: ${rootURI}`);
+    }
+
+    shutdown() {
+      Zotero.Notifier.unregisterObserver(this.tabObserverID);
+      Services.console.logStringMessage(`MyPlugin with id: ${this.id} is shutting down.`);
+    }
+  };
+  
+  MyPlugin.init({ id, version, rootURI });
+}
+
+function shutdown() {
+  MyPlugin.shutdown();
+  MyPlugin = undefined;
+}
+
+async function _addTabHighlights(tabId) {
+  Services.console.logStringMessage(`[MyPlugin] Adding highlights for tab: ${tabId}`);
+  let reader = Zotero.Reader.getByTabID(tabId);
+  if (reader) {
+    Services.console.logStringMessage(`[MyPlugin] Found reader for tab: ${tabId}`);
+    // Wait for the reader to be fully initialized
+    await reader._initPromise;
+    let pdfIframe = reader._iframeWindow.document.querySelector('iframe');
+    let innerFrame = reader._iframeWindow.wrappedJSObject;
+    let innerReader = innerFrame._reader;
+    let pdfWindow = pdfIframe ? pdfIframe.contentWindow : null;
+    if (pdfWindow) {
+      Services.console.logStringMessage(`[MyPlugin] Found PDF window for tab: ${tabId}`);
+      while (!pdfWindow.wrappedJSObject.PDFViewerApplication) {
+        await new Promise(r => setTimeout(r, 50));
+      }
+      let pdfApp = pdfWindow.wrappedJSObject.PDFViewerApplication;
+      await pdfApp.initializedPromise;
+      Services.console.logStringMessage(`[MyPlugin] Found PDFViewerApplication for tab: ${tabId}`);
+      _applyHighlights(pdfApp, innerReader);  
+    }
+  }
+}
+
+async function _applyHighlights(pdfApp, innerReader) {
+  // To highlight I need a set of words I want to highlight and then I need to go find them in the pdf
+  // and get their rects so I can add them to the set of highlights in the document. On tab closing, I should clean up.
+  const pagesCount = pdfApp.pdfViewer.pagesCount;
+  Services.console.logStringMessage(`[MyPlugin] Starting highlighting for tab: ${tabId} (${pagesCount} pages found)`);
+
+  const highlightAnnotations = [];
+  for (let i = 0; i < pagesCount; i++) {
+    // Per page:
+    // 1. Get the text content of the page (which has positions)
+    // 2. For each text content item, get the transform matrix (to convert into pdf space)
+    // 3. Find the items that match the text we want to highlight and build the rect.
+    Services.console.logStringMessage(`[MyPlugin] Starting highlighting for tab: ${tabId} (page ${i + 1})`);
+    const page = await pdfApp.pdfDocument.getPage(i + 1); // 1-indexed
+    const textContent = await page.wrappedJSObject.getTextContent();
+  }
+
+  const currentAnnotations = innerReader._state.annotations;
+  innerReader.setAnnotations([...currentAnnotations, highlightAnnotations]);
+  myPlugin.unsetAnnotations = innerReader.unsetAnnotations.bind(innerReader);
+      // to set / unset annotations:
+    /* innerReader.setAnnotations([{
+        "libraryID": 1
+        "type": "highlight"
+        "isExternal": ""
+        "readOnly": ""
+        "text": "enhancing programming education"
+        "comment": ""
+        "pageLabel": "12"
+        "color": "#5fb236"
+        "sortIndex": "00001|000663|00345"
+        "position": {
+            "pageIndex": 1
+            "rects": [
+                "0": [
+                    "0": 53.798
+                    "1": 438.447
+                    "2": 185.954
+                    "3": 446.391
+                ]
+            ]
+        }
+        "dateModified": "2026-03-11T19:49:51Z"
+        "id": "TTARCENY"
+        "tags": []
+      }])
+    */
+}
